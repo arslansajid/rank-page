7186421e02887e571a3491d7a8f5b4c6da9dc31e
Arslan Sajid <arslansajid951@gmail.com>
2020-07-14 21:25:20 +0500

    static files added

---
+++ src/static/_data.js
@@ -0,0 +1,55 @@
+
+export const videos = [
+    {
+        id: "8830383",
+        url: 'https://picsum.photos/id/1018/1000/600/',
+        title: "Small Wooden"
+    },
+    {
+        id: "8830383",
+        url: 'https://picsum.photos/id/1015/1000/600/',
+        title: "Intelligent Hat"
+    },
+    {
+        id: "8830383",
+        url: 'https://picsum.photos/id/1019/1000/600/',
+        title: "Steel Chicken"
+    },
+    {
+        id: "8830383",
+        url: 'https://picsum.photos/id/1029/1000/600/',
+        title: "Small Bacon"
+    },
+    {
+        id: "8830383",
+        url: 'https://picsum.photos/id/1045/1000/600/',
+        title: "Gorgeous Pizza"
+    },
+    {
+        id: "8830383",
+        url: 'https://picsum.photos/id/1074/1000/600/',
+        title: "Rustic Car"
+    }
+];
+
+
+export const news = [
+    {
+        id: "8830383",
+        url: 'https://picsum.photos/id/1022/1000/600/',
+        title: "Ms. Ila Von",
+        role: "Viewer"
+    },
+    {
+        id: "8830383",
+        url: 'https://picsum.photos/id/1023/1000/600/',
+        title: "Cyrus Jakubowski",
+        role: "Admin"
+    },
+    {
+        id: "8830383",
+        url: 'https://picsum.photos/id/1024/1000/600/',
+        title: "Lola Lehner",
+        role: "Admin"
+    }
+];
---
+++ src/static/_nav.js
@@ -0,0 +1,17 @@
+
+export default {
+  items: [
+    {
+      name: 'Home',
+      url: '/',
+    },
+    {
+      name: 'Matches',
+      url: '/matches',
+    },
+    {
+      name: 'Contact Us',
+      url: '/contact-us',
+    },
+  ]
+}

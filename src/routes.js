5787e9b3e13a22cb94f3ee35ad522cdb3c8a01f8
Arslan Sajid <arslansajid951@gmail.com>
2020-07-14 21:25:38 +0500

    functional component structrure

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Configure routes
@@ -6,12 +6,16 @@ const Home = React.lazy(() => import('./containers/Home'));
 const About = React.lazy(() => import('./containers/About'));
 const PageNotFound = React.lazy(() => import('./containers/PageNotFound'));
 
-export default (
-  <Suspense fallback={<div>Loading...</div>}>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/about" component={About}/>
-      <Route exact path='*' component={PageNotFound} />
-    </Switch>
-  </Suspense>
-);
+const Routes = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path='*' component={PageNotFound} />
+      </Switch>
+    </Suspense>
+  )
+}
+
+export default Routes;

637be62802ae7f5a9c81f824ddaf2876d64e46c7
Arslan Sajid <arslansajid951@gmail.com>
2020-07-14 22:23:05 +0500

    added api configs

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+
+
+const Config = {
+  BASE_APP_URL: 'https://BASE_URL_GOES_HERE.com/api/',
+  API_VERSION: 'v1',
+};
+
+Config.API_END_POINT = Config.BASE_APP_URL + Config.API_VERSION;
+
+export default Config;

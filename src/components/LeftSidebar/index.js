15231f8627838c5fe0eeceeab1586befe9cb7c15
Arslan Sajid <arslansajid951@gmail.com>
2020-07-15 20:58:15 +0500

    updated left sidebar

--- src/components/LeftSidebar/index.js
+++ src/components/LeftSidebar/index.js
@@ -1,33 +1,50 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
+import SidebarCard from "../SidebarCard";
 import Dialog from "../Common/Dialog";
 import SignIn from "../SignIn";
+import Colors from '../../static/_colors';
 
 const LeftSidebar = (props) => {
+    const classes = useStyles();
     const [showSignIn, setShowSignIn] = useState(false);
 
     return (
         <>
-        {
-            showSignIn && (
-                <Dialog
-                    title={"Sign In"}
-                    open={showSignIn}
-                    message={<SignIn />}
-                    applyForm={() => setShowSignIn(false)}
-                    cancelForm={() => setShowSignIn(false)}
-                    hideActions={true}
-                />
-            )
-        }
-        <Button
-            onClick={() => setShowSignIn(true)}
-            variant="contained" color="primary">
-            Sign in
-        </Button>
+            {
+                showSignIn && (
+                    <Dialog
+                        title={"Sign In"}
+                        open={showSignIn}
+                        message={<SignIn />}
+                        applyForm={() => setShowSignIn(false)}
+                        cancelForm={() => setShowSignIn(false)}
+                        hideActions={true}
+                    />
+                )
+            }
+            <div className={classes.main}>
+                <Button
+                    onClick={() => setShowSignIn(true)}
+                    variant="contained" color="primary">
+                    Sign in
+                </Button>
+                <SidebarCard />
+                <SidebarCard />
+                <SidebarCard />
+                <SidebarCard />
+            </div>
         </>
     )
 }
+
+const useStyles = makeStyles((theme) => ({
+    main: {
+        background: Colors.white,
+        height: '100%',
+        padding: '1em 1em 0 3em'
+    }
+}))
 
 export default LeftSidebar;

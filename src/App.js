f01ce607f52d79d976ca700461ca35227681e936
Arslan Sajid <arslansajid951@gmail.com>
2020-07-16 01:33:24 +0500

    app indenetation

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Routes from './routes';
 
 // header and footer
 import Header from './containers/Header'
-import { Grid, Container } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 import { MuiThemeProvider, StylesProvider } from '@material-ui/core';
 import { theme } from './utils/MuiTheme';
@@ -23,34 +23,34 @@ import { theme } from './utils/MuiTheme';
 import LeftSidebar from './components/LeftSidebar';
 import RightSidebar from './components/RightSidebar';
 const App = () => {
-    const classes = useStyles();
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <MuiThemeProvider theme={theme}>
-            <StylesProvider injectFirst>
-                <Grid className={classes.main} container>
-                  <Grid className={classes.leftSidebar} item lg={2} md={2} sm={false} xs={false}>
-                    <LeftSidebar />
-                  </Grid>
-                  <Grid className={classes.routeContainer} item lg={8} md={8} sm={12} xs={12}>
-                    <Header />
-                    <Routes />
-                  </Grid>
-                  <Grid className={classes.rightSidebar} item lg={2} md={2} sm={false} xs={false}>
-                    <RightSidebar />
-                  </Grid>
-                  </Grid>
-            </StylesProvider>
-          </MuiThemeProvider>
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
+  const classes = useStyles();
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <MuiThemeProvider theme={theme}>
+          <StylesProvider injectFirst>
+            <Grid className={classes.main} container>
+              <Grid className={classes.leftSidebar} item lg={2} md={2} sm={false} xs={false}>
+                <LeftSidebar />
+              </Grid>
+              <Grid className={classes.routeContainer} item lg={8} md={8} sm={12} xs={12}>
+                <Header />
+                <Routes />
+              </Grid>
+              <Grid className={classes.rightSidebar} item lg={2} md={2} sm={false} xs={false}>
+                <RightSidebar />
+              </Grid>
+            </Grid>
+          </StylesProvider>
+        </MuiThemeProvider>
+      </ConnectedRouter>
+    </Provider>
+  );
+}
 
 const useStyles = makeStyles((theme) => ({
   main: {
-      background: Colors.background,
+    background: Colors.background,
   },
   routeContainer: {
     padding: '0 1em',

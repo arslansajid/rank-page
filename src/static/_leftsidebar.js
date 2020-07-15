83b70fcda947ed04982a1de4370209c342992afa
Arslan Sajid <arslansajid951@gmail.com>
2020-07-15 21:16:15 +0500

    added static

---
+++ src/static/_leftsidebar.js
@@ -0,0 +1,27 @@
+export const Menu = [
+    {
+        name: 'Newsfeed',
+        icon: 'home',
+        route: '/newsfeed'
+    },
+    {
+        name: 'Notifications',
+        icon: 'home',
+        route: '/notifications'
+    },
+    {
+        name: 'Categories',
+        icon: 'home',
+        route: '/categories'
+    },
+    {
+        name: 'Messages',
+        icon: 'home',
+        route: '/messages'
+    },
+    {
+        name: 'Competitions',
+        icon: 'home',
+        route: '/competitions'
+    },
+]

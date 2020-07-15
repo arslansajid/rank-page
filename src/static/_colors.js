acabd5bf4308cec1f4d07740ea196b059cfa2516
Arslan Sajid <arslansajid951@gmail.com>
2020-07-15 20:57:55 +0500

    Added colors

---
+++ src/static/_colors.js
@@ -0,0 +1,17 @@
+const Colors = {
+    white: "#FFFFFF",
+    black: "#333333",
+    blue: "#6574FB",
+    brandColor: '#707070',
+    appRed: "#2e8b57",
+    border: "#E9EBEE",
+    headingBlue: "#102B5C",
+    newsLetterBg: "#FFECEE",
+    textGrey: "#8E8E8E",
+    placeHolderText: "#B4B4B4",
+    inputBg: "#F6F8FC",
+    background: "#FBFBFB",
+  };
+  
+  export default Colors;
+  

153927219554f87656a556bd7a6948e880917505
Arslan Sajid <arslansajid951@gmail.com>
2020-07-15 20:56:45 +0500

    right sidebar updated

--- src/components/RightSidebar/index.js
+++ src/components/RightSidebar/index.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
-import Dialog from "../Common/Dialog";
+import SidebarCard from "../SidebarCard";
+import Colors from '../../static/_colors';
 
 const RightSidebar = (props) => {
-
+    const classes = useStyles();
     return (
         <>
-            Right Sidebar
+            <div className={classes.main}>
+                <SidebarCard />
+                <SidebarCard />
+                <SidebarCard />
+                <SidebarCard />
+            </div>
         </>
     )
 }
+
+const useStyles = makeStyles((theme) => ({
+    main: {
+        background: Colors.white,
+        height: '100%',
+        padding: '1em'
+    }
+}))
 
 export default RightSidebar;

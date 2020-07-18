8803bafe889232bbf9f387652906072d8cd3c3d1
Arslan Sajid <arslansajid951@gmail.com>
2020-07-18 22:18:00 +0500

    messages route

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const Home = React.lazy(() => import('./containers/Home'));
 const About = React.lazy(() => import('./containers/About'));
 const SignUp = React.lazy(() => import('./components/SignUp'));
 const Profile = React.lazy(() => import('./containers/Profile'));
-const Chat = React.lazy(() => import('./containers/Chat'));
+const Messages = React.lazy(() => import('./containers/Chat'));
 const Settings = React.lazy(() => import('./containers/Settings'));
 const EditProfile = React.lazy(() => import('./containers/EditProfile'));
 const PageNotFound = React.lazy(() => import('./containers/PageNotFound'));
@@ -26,7 +26,7 @@ const Routes = () => {
         <Route exact path="/about" render={() => <About />} />
         <Route exact path="/SignUp" render={() => <SignUp />} />
         <Route exact path="/profile" render={() => <Profile />} />
-        <Route exact path="/chat" render={() => <Chat />} />
+        <Route exact path="/messages" render={() => <Messages />} />
         <Route exact path="/settings" render={() => <Settings />} />
         <Route exact path="/edit-profile" render={() => <EditProfile />} />
         {/* <Route exact path="/about" render={() => <About />} /> */}

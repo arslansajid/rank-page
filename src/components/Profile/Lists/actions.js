281a68e99883743601db897e36dd6ce5b0ecbd74
ZeeshanGraana <Zeeshan.sarwar@graana.com>
2020-07-27 02:00:53 +0500

    custon react select added

--- src/components/Profile/Lists/actions.js
+++ src/components/Profile/Lists/actions.js
@@ -4,21 +4,19 @@ import axiosInstance from '../../../api/api.config';
 export const GetListItems = async () => {
     try {
       const res = await axiosInstance.get(
-        `${Config.API_END_POINT}/follow_list_items`);
+        `${Config.API_END_POINT}/list_items`);
       return res;
     } catch (err) {
       throw err.response;
     }
-    // axiosInstance.get(`${Config.API_END_POINT}/follow_list_items`).then
   };
 
   export const PostListItem = async (list_item) => {
     try {
       const res = await axiosInstance.get(
-        `${Config.API_END_POINT}/list_items` , {list_item});
+        `${Config.API_END_POINT}/create_list` , {list_item});
       return res;
     } catch (err) {
       throw err.response;
     }
-    // axiosInstance.get(`${Config.API_END_POINT}/follow_list_items`).then
   };

5e01f6b22b61d1d81b230a5210d81ce2b2f84127
Arslan Sajid <arslansajid951@gmail.com>
2020-07-26 16:45:19 +0500

    test for config

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -1,5 +1,7 @@
 import Config from './config';
 import axios from 'axios';
+import Cookie from "js-cookie";
+const token = Cookie.get('rankpage_access_token')
 
 const axiosInstance = axios.create({
   baseURL: Config.API_END_POINT,
@@ -7,6 +9,7 @@ const axiosInstance = axios.create({
   // withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
+    'Authorization' : !!token ? token : '',
   },
 });
 

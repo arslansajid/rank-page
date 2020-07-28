00dd9b97ac91ef223199a9e1e337b8df0d7cdf8d
Arslan Sajid <arslansajid951@gmail.com>
2020-07-28 23:28:06 +0500

    Profile tabs routing fixed

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -5,70 +5,77 @@ import ProfileCover from "../../components/Profile/Cover";
 import Lists from "../../components/Profile/Lists";
 import Challenges from "../../components/Profile/Challenges";
 import Categories from "../../components/Profile/Categories";
-import {withRouter} from "react-router-dom"
+import { withRouter, Switch, Route } from "react-router-dom";
 
 const Profile = (props) => {
     const classes = useStyles();
     const [activeTab, setActiveTab] = useState(null);
 
-    const handleTabChange = (value) => {
-        setActiveTab(value)
-    }
-
     useEffect(() => {
-        console.log('withRouter', props);
-        if(props.match.params.tab === "lists") {
+        if (props.match.params.tab === "lists") {
             setActiveTab(1)
         } else if (props.match.params.tab === "challenges") {
             setActiveTab(2)
-        } else if(props.match.params.tab === "categories") {
+        } else if (props.match.params.tab === "categories") {
             setActiveTab(3)
         }
     }, [])
 
-		return (
-			<div className={classes.main}>
-                <ProfileCover />
-				<ButtonGroup fullWidth size ='large'>
-                    <Button  className ={classes.buttons} onClick={() => handleTabChange(1)}>
-                        <Typography className={activeTab === 1 ? classes.tabselected : classes.tab}>Lists</Typography>
-                    </Button>
-                    <Button onClick={() => handleTabChange(2)}>
-                        <Typography className={activeTab === 2 ? classes.tabselected : classes.tab}>Challenges</Typography>
-                    </Button>
-                    <Button onClick={() => handleTabChange(3)}>
-                        <Typography className={activeTab === 3 ? classes.tabselected : classes.tab}>Categories</Typography></Button>
-                </ButtonGroup>
-                {
-                    activeTab === 1 ? (
-                        <>
-                            <Lists />
-                        </>
-                    )
-                    :
-                    activeTab === 2 ? (
-                        <>
-                            <Challenges />
-                        </>
-                    )
-                    :
-                    <>
-                        <Categories />
-                    </>
-                }
-                
-			</div>
-		);
+    const onTabChangeHandler = (selected) => {
+        setActiveTab(selected);
+        if(selected === 1) {
+            props.history.push('/profile/lists');
+        } else if(selected === 2) {
+            props.history.push('/profile/challenges');
+        } else if(selected === 3) {
+            props.history.push('/profile/categories');
+        }
     }
-    
+
+    return (
+        <div className={classes.main}>
+            <ProfileCover />
+            <ButtonGroup fullWidth size='large'>
+                <Button className={classes.buttons} onClick={() => onTabChangeHandler(1)}>
+                    <Typography className={activeTab === 1 ? classes.tabselected : classes.tab}>Lists</Typography>
+                </Button>
+                <Button onClick={() => onTabChangeHandler(2)}>
+                    <Typography className={activeTab === 2 ? classes.tabselected : classes.tab}>Challenges</Typography>
+                </Button>
+                <Button onClick={() => onTabChangeHandler(3)}>
+                    <Typography className={activeTab === 3 ? classes.tabselected : classes.tab}>Categories</Typography></Button>
+            </ButtonGroup>
+
+            <Grid>
+                <Switch>
+                    <Route
+                        path="/profile/lists"
+                        render={props => (
+                            <Lists {...props} />
+                        )}
+                    />
+                    <Route
+                        path="/profile/challenges"
+                        render={props => <Challenges {...props} />}
+                    />
+                    <Route
+                        path="/profile/categories"
+                        render={props => <Categories {...props} />}
+                    />
+                </Switch>
+            </Grid>
+        </div>
+    );
+}
+
 const useStyles = makeStyles((theme) => ({
     main: {
-		marginBottom: 20,
+        marginBottom: 20,
 
-		[theme.breakpoints.down('sm')]: {
-			marginBottom: 70,
+        [theme.breakpoints.down('sm')]: {
+            marginBottom: 70,
         },
-	},
+    },
     tabSelected: {
         fontWeight: 400
     },
@@ -79,7 +86,7 @@ const useStyles = makeStyles((theme) => ({
         padding: '12px 21px',
         fontSize: '1rem'
     },
-    })
+})
 )
 
 export default withRouter(Profile);

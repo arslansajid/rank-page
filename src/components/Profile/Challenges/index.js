d38d020c885c448a808c1059110d84bdbc78d349
Arslan Sajid <arslansajid951@gmail.com>
2020-08-09 11:55:25 +0500

    lists/challenges

--- src/components/Profile/Challenges/index.js
+++ src/components/Profile/Challenges/index.js
@@ -1,20 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Typography, Grid } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import PostCard from "../../PostCard";
+import { connect } from "react-redux";
+import LoadingSpinner from "../../Common/LoadingSpinner"
+import {getChallenges} from "./actions"
 
-const Challenges = () => {
+const Challenges = (props) => {
     const classes = useStyles();
+    const [challenges, setChallenges] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const { user } = props;
+
+    useEffect(() => {
+        if (!!user) {
+            getChallenges({ 'user_id': user.id })
+                .then((res) => {
+                    console.log('res', res)
+                    setChallenges(res.data.data.users_own_lists)
+                    setIsLoading(false);
+                })
+                .catch((err) => {
+                    console.log('err', err);
+                    setIsLoading(false);
+                })
+        }
+    }, [user])
 
     return (
         <>
-            {[...Array(3)].map((list, index) => {
+            {
+                isLoading && (
+                    <LoadingSpinner
+                        loading={isLoading}
+                        text="Fetching Challenges..."
+                        size="large"
+                    />
+                )
+            }
+            {challenges.length
+            ?
+            challenges.map((challenge, index) => {
                 return (
                     <Grid key={index}>
-                        {/* <PostCard /> */}
+                        <PostCard post={challenge} />
                     </Grid>
                 )
-            })}
+            })
+        :
+        "No Challenges Present"
+        }
         </>
     )
 
@@ -28,5 +64,10 @@ const useStyles = makeStyles((theme) => ({
 })
 )
 
+function mapStateToProps(state) {
+    return {
+        user: state.user,
+    };
+}
 
-export default Challenges;
+export default connect(mapStateToProps)(Challenges);

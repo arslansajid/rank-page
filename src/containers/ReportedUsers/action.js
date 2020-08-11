aa14ea398c401964be98db67cce6ea6b250c6d43
Arslan Sajid <arslansajid951@gmail.com>
2020-08-11 21:59:43 +0500

    Reported users page

---
+++ src/containers/ReportedUsers/action.js
@@ -0,0 +1,13 @@
+
+import Config from '../../api/config';
+import axiosInstance from '../../api/api.config';
+
+  export const getReportedUsers = async (data) => {
+    try {
+      const res = await axiosInstance.post(
+        `${Config.API_END_POINT}/rearrange_list`, data);
+      return res;
+    } catch (err) {
+      throw err.response;
+    }
+  };

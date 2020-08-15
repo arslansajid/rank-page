4388c4244e05329baea3e039edfb5d8360ae674b
Arslan Sajid <arslansajid951@gmail.com>
2020-08-15 11:47:29 +0500

    Fans and followers pages

--- src/containers/Followers/index.js
+++ src/containers/Followers/index.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Paper, Grid, Typography } from '@material-ui/core';
+import { Paper, Grid, Typography, Button, ButtonGroup } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import LoadingSpinner from "../../components/Common/LoadingSpinner"
 import Colors from "../../static/_colors";
-import BlockTile from "../../components/BlockTile";
+import UserTile from "../../components/Search/UserTile";
 import { getFollowers } from "./action";
 
 const ReportedUsers = () => {
     const classes = useStyles();
     const [followers, setFollowers] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [activeTab, setActiveTab] = useState(1);
 
     useEffect(() => {
         getFollowers()
@@ -24,6 +25,10 @@ const ReportedUsers = () => {
             })
     }, [])
 
+    const handleTabChange = (value) => {
+        setActiveTab(value)
+    }
+
     if (isLoading) {
         return (
             <LoadingSpinner
@@ -35,11 +40,28 @@ const ReportedUsers = () => {
     } else {
         return (
             <>
+            <ButtonGroup className={classes.buttons} fullWidth size='large'>
+                <Button onClick={() => handleTabChange(1)}>
+                    <Typography className={activeTab === 1 ? classes.tabselected : classes.tab}>All</Typography>
+                </Button>
+                <Button onClick={() => handleTabChange(2)}>
+                    <Typography className={activeTab === 2 ? classes.tabselected : classes.tab}>Users</Typography>
+                </Button>
+                <Button onClick={() => handleTabChange(3)}>
+                    <Typography className={activeTab === 3 ? classes.tabselected : classes.tab}>Experts</Typography>
+                </Button>
+                <Button onClick={() => handleTabChange(4)}>
+                    <Typography className={activeTab === 4 ? classes.tabselected : classes.tab}>Pages</Typography>
+                </Button>
+            </ButtonGroup>
+
+            {activeTab === 1 && (
+                <>
                 <Paper elevation={0} className={classes.container}>
                     {followers.length > 0 ? followers.map((follower, index) => {
                         return (
                             <Grid key={index}>
-                                <BlockTile name={follower.name} userName={follower.user_name} showButton={false} />
+                                <UserTile userId={follower.id} name={follower.name} userName={follower.user_name} />
                             </Grid>
                         )
                     })
@@ -47,6 +69,26 @@ const ReportedUsers = () => {
                         <Typography variant="h5">No Followers Yet!</Typography>
                     }
                 </Paper>
+                </>
+            )}
+
+                {activeTab === 2 && (
+                        <>
+                            <Paper elevation={0} className={classes.container}>
+                                <Typography variant="h6" gutterBottom>Users</Typography>
+                                {followers.length > 0 ? followers.map((follower, index) => {
+                                    return (
+                                        <Grid key={index}>
+                                            <UserTile userId={follower.id} name={follower.name} userName={follower.user_name} />
+                                        </Grid>
+                                    )
+                                })
+                                :
+                                <Typography variant="h6" gutterBottom>No Users found...</Typography>
+                            }
+                            </Paper>
+                        </>
+                    )}
             </>
         );
     }
@@ -66,7 +108,12 @@ const useStyles = makeStyles((theme) => ({
         width: '100%',
         height: 1,
         margin: "1em 0 1em"
-    }
+    },
+    buttons: {
+        margin: theme.spacing(1, 0, 1, 0),
+        fontSize: '1rem',
+        borderRadius: '8px 0px 0px 8px',
+    },
 })
 )
 

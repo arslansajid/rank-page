fb407b4003f639781a1769f1eb83956df62996e2
Arslan Sajid <arslansajid951@gmail.com>
2020-08-16 18:34:17 +0500

    fix for scroll to top on like popover close

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -202,7 +202,7 @@ const PostCard = (props) => {
                 horizontal: 'left',
                 }}
                 onClose={e => handlePopoverClose(e)}
-                disableRestoreFocus
+                disableRestoreFocus //this was the fix for scroll to top on like
             >
                 {/* <ClickAwayListener onClickAway={handlePopoverClose}> */}
                     <Grid className={classes.reactContainer} component={Paper} elevation={1} container justify="space-between">

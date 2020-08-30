145a4ab4e9d15649d7107b5effd571a0478b6238
Arslan Sajid <arslansajid951@gmail.com>
2020-08-30 15:21:39 +0500

    bottom navigation updated

--- src/components/BottomNavigation/index.js
+++ src/components/BottomNavigation/index.js
@@ -5,6 +5,7 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import HomeIcon from '@material-ui/icons/Home';
 import ExploreIcon from '@material-ui/icons/Explore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import MessageIcon from '@material-ui/icons/MailOutline';
 import AccountIcon from '@material-ui/icons/AccountCircle';
 import ListIcon from '@material-ui/icons/List';
 
@@ -26,8 +27,8 @@ const BottomNavigationBar = (props) => {
   return (
     <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
       <BottomNavigationAction label="Home" value="/" icon={<HomeIcon />} />
-      <BottomNavigationAction label="Explore" value="/explore" icon={<ExploreIcon />} />
-      <BottomNavigationAction label="Favorites" value="/favorites" icon={<FavoriteIcon />} />
+      {/* <BottomNavigationAction label="Explore" value="/explore" icon={<ExploreIcon />} /> */}
+      <BottomNavigationAction label="Messages" value="/messages" icon={<MessageIcon />} />
       <BottomNavigationAction label="Menu" value="/menu" icon={<ListIcon />} />
       <BottomNavigationAction label="Profile" value="/profile/lists" icon={<AccountIcon />} />
     </BottomNavigation>
